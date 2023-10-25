8c1e1d5b68d75e9c6124c48e6d483e81984045aa
DreamyTZK <i@tzki.cn>
2023-10-25 22:05:13 +0800

    feat: lodash-es demo

--- src/components/page-client.tsx
+++ src/components/page-client.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useRequest } from 'ahooks'
+import { debounce } from 'lodash-es'
 import { useState } from 'react'
 
 import hitokotoApi from '@/service/hitokoto'
@@ -18,7 +19,7 @@ export default function ClientPage(options: { hitokoto: string }) {
     <>
       {hitokoto}
       <div className="cursor-pointer">
-        <button className="btn" onClick={() => run()}>刷新</button>
+        <button className="btn" onClick={debounce(() => run(), 200)}>刷新</button>
       </div>
     </>
   )
