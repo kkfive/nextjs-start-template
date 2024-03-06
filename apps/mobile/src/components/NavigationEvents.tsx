a7eb659c9babc75807e3cdf8e4493f579fbf5f29
DreamyTZK <i@tzki.cn>
2024-03-06 14:55:45 +0800

    perf: 去除客户端路由刷新

--- apps/mobile/src/components/NavigationEvents.tsx
+++ apps/mobile/src/components/NavigationEvents.tsx
@@ -6,11 +6,11 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 export function NavigationEvents() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
-  const router = useRouter()
+  // const router = useRouter()
 
   useEffect(() => {
-    if (pathname === '/goods/1')
-      router.refresh()
+    // if (pathname === '/goods/1')
+    // router.refresh()
   }, [pathname, searchParams])
 
   return null
--- apps/mobile/src/services/mock/api.ts
+++ apps/mobile/src/services/mock/api.ts
@@ -10,11 +10,9 @@ export async function goodsDetailApi(commonId: string, goodsId?: string | null) 
   const result = await request<GoodsDetail>('https://mock.apifox.com/m1/4081920-0-default/goods', {
     method: 'post',
     body: urlencoded,
-    // cache: 'no-store',
+    cache: 'no-store',
     next: {
       tags: ['goodsDetailApi'],
-      revalidate: 10,
-
     },
   })
   return result
