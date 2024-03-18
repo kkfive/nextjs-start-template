3e17f6c5b2abe87a1ea230ede74f8d5bf5a050ee
DreamyTZK <i@tzki.cn>
2024-03-18 13:44:52 +0800

    chore: 优化代码风格

--- apps/web/src/app/microweb/[brandName]/[...slug]/page.tsx
+++ apps/web/src/app/microweb/[brandName]/[...slug]/page.tsx
@@ -5,9 +5,7 @@ export default function Page({ params }: { params: { brandName: string, slug: st
   const brandName = params.brandName
   const path = params.slug.join('/').replace('.html', '')
 
-  const DynamicComponent = dynamic(() => import(`@kkproject/brand-${brandName}/web/app/${path}/page.tsx`).catch(() => {
-    notFound()
-  }))
+  const DynamicComponent = dynamic(() => import(`@kkproject/brand-${brandName}/web/app/${path}/page.tsx`).catch(() => notFound()))
 
   return <DynamicComponent />
 }
