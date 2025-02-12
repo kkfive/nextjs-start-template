834f1594aa546266922107c4e98b0195dd86c43e
DreamyTZK <texon233@gmail.com>
2025-02-12 14:38:00 +0800

    chore: 调整 vscode tailwindcss 支持

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,6 @@ import { addIconSelectors } from '@iconify/tailwind'
 const allIconSets = Object.keys(iconifyJSONData)
 
 export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './domain/**/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
   theme: {
     extend: {
       colors: {
